462c6362c83b96ab80f41411e80d0c2feed38c9b
S. M. Hickman <sean@grump.ly>
2017-08-20 23:05:34 -0500

    Add travis and nix configs.

---
+++ trivial.nix
@@ -0,0 +1,16 @@
+{ mkDerivation, async, base, containers, deepseq, directory
+, ghc-prim, hashable, managed, mtl, pretty-show, random, stdenv
+, stm, template-haskell, unordered-containers
+}:
+mkDerivation {
+  pname = "trivial";
+  version = "0.1.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    async base containers deepseq directory ghc-prim hashable managed
+    mtl pretty-show random stm template-haskell unordered-containers
+  ];
+  homepage = "github.com/grumply/champ";
+  description = "Simple benchmarking and Easy testing";
+  license = stdenv.lib.licenses.bsd3;
+}
