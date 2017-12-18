4842068d91a422e56aa0c317d42e1b8c945a3a4d
grumply <sean@grump.ly>
2017-12-18 14:02:05 -0600

    Simplify nix expressions.

--- default.nix
+++ default.nix
@@ -1,26 +1,16 @@
-{ compiler ? "ghc801" }:
-
-let
-  config = {
-    packageOverrides = pkgs: rec {
-      haskell = pkgs.haskell // {
-        packages = pkgs.haskell.packages // {
-          "${compiler}" = pkgs.haskell.packages."${compiler}".override {
-            overrides = new: old: rec {
-
-              trivial =
-                new.callPackage ./trivial.nix { };
-
-            };
-          };
-        };
-      };
-    };
-  };
-
-  pkgs = import <nixpkgs> { inherit config; };
-
-in
-  { trivial = pkgs.haskell.packages.${compiler}.trivial;
-  }
-
+{ mkDerivation, aeson, async, base, deepseq, directory, ghc-prim
+, hashable, mtl, random, stdenv, stm, text, transformers
+, unordered-containers, vector
+}:
+mkDerivation {
+  pname = "trivial";
+  version = "0.1.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson async base deepseq directory ghc-prim hashable mtl random stm
+    text transformers unordered-containers vector
+  ];
+  homepage = "github.com/grumply/trivial";
+  description = "Simple benchmarking and Easy testing for GHC and GHCJS";
+  license = stdenv.lib.licenses.bsd3;
+}
